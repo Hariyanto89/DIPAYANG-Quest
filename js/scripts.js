8eaf3e2997e07d0f9b2ad4460e520f80043d7d46
Hariyanto <yantonft@gmail.com>
2024-12-14 16:39:29 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,23 @@ document.addEventListener('DOMContentLoaded', () => {
         { img: "assets/badges/merigi_badge6.png", title: "Judul Tugas 6" },
         { img: "assets/badges/merigi_badge7.png", title: "Judul Tugas 7" },
         { img: "assets/badges/merigi_badge.png", title: "Petani Aseters" },
+        { img: "assets/badges/merigi_badge.png", title: "Petani Aseters" },
+        { img: "assets/badges/merigi_badge2.png", title: "Judul Tugas 2" },
+        { img: "assets/badges/merigi_badge3.png", title: "Judul Tugas 3" },
+        { img: "assets/badges/merigi_badge4.png", title: "Judul Tugas 4" },
+        { img: "assets/badges/merigi_badge5.png", title: "Judul Tugas 5" },
+        { img: "assets/badges/merigi_badge6.png", title: "Judul Tugas 6" },
+        { img: "assets/badges/merigi_badge7.png", title: "Judul Tugas 7" },
+        { img: "assets/badges/merigi_badge.png", title: "Petani Aseters" },
+        { img: "assets/badges/merigi_badge2.png", title: "Judul Tugas 2" },
+        { img: "assets/badges/merigi_badge3.png", title: "Judul Tugas 3" },
+        { img: "assets/badges/merigi_badge4.png", title: "Judul Tugas 4" },
+        { img: "assets/badges/merigi_badge5.png", title: "Judul Tugas 5" },
+        { img: "assets/badges/merigi_badge6.png", title: "Judul Tugas 6" },
+        { img: "assets/badges/merigi_badge7.png", title: "Judul Tugas 7" },
+        { img: "assets/badges/merigi_badge.png", title: "Petani Aseters" },
+        { img: "assets/badges/merigi_badge7.png", title: "Judul Tugas 7" },
+        { img: "assets/badges/merigi_badge.png", title: "Petani Aseters" },
     ];
 
     function renderCards() {

0d06e341f6db6bcf55ec5aacfab5557cbe078537
Hariyanto <yantonft@gmail.com>
2024-12-14 16:10:08 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
         { img: "assets/badges/merigi_badge6.png", title: "Judul Tugas 6" },
         { img: "assets/badges/merigi_badge7.png", title: "Judul Tugas 7" },
         { img: "assets/badges/merigi_badge.png", title: "Petani Aseters" },
-        { img: "assets/badges/merigi_badge2.png", title: "Judul Tugas 2" },
-        { img: "assets/badges/merigi_badge3.png", title: "Judul Tugas 3" },
-        { img: "assets/badges/merigi_badge4.png", title: "Judul Tugas 4" },
-        { img: "assets/badges/merigi_badge5.png", title: "Judul Tugas 5" },
-        { img: "assets/badges/merigi_badge6.png", title: "Judul Tugas 6" },
       ];
 
     // Loop through the array to create the cards

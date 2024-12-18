8f6fb6d5425f6342853892a41715ab3c47c7bb23
Hariyanto <yantonft@gmail.com>
2024-12-18 11:34:05 +0700

    Update game1.js

--- js/game1.js
+++ js/game1.js
@@ -1,3 +1,36 @@
+// firebase.js
+// Mengimpor modul Firebase dari CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+// Konfigurasi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAw6mmGUrDyFiVoRdejZZJ7U_Cp7Od-8aI",
+  authDomain: "dipayang-quest.firebaseapp.com",
+  projectId: "dipayang-quest",
+  storageBucket: "dipayang-quest.appspot.com", // Dikoreksi agar sesuai dengan format Firebase Storage
+  messagingSenderId: "769511051480",
+  appId: "1:769511051480:web:76606871be7200ce3047ad",
+  measurementId: "G-BEP9KDRTCG"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inisialisasi Firestore untuk database
+const db = getFirestore(app);
+
+// Inisialisasi Storage untuk file seperti gambar atau dokumen
+const storage = getStorage(app);
+
+// Inisialisasi Authentication untuk login pengguna (opsional)
+const auth = getAuth(app);
+
+// Mengekspor modul Firebase agar bisa digunakan di file lain
+export { db, storage, auth };
+
 import { db } from './firebase.js';
 import { collection, getDocs, addDoc, updateDoc, doc } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
 

6887c6f36e415f7c08127990c4d3fd3255f7f4af
Hariyanto <yantonft@gmail.com>
2024-12-19 19:11:56 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
 // Inisialisasi data tugas statis
 const staticTasks = [
-  { img: "assets/icon/projectmanagement.png", title: "Project Management", link: "https://hariyanto89.github.io/Quest.ID/tasks/ProjectManagement.html" },
+  { img: "assets/icon/projectmanagement.png", title: "Project Management", link: "https://hariyanto89.github.io/Quest.ID/tasks/project-management-task1.html" },
   { img: "assets/icon/gamechapter2.jpg", title: "Gelombang Aset Tani", link: "game2.html" },
   { img: "assets/icon/gamechapter3.jpg", title: "Menghias Aset Tani", link: "game3.html" },
   { img: "assets/badges/merigi_badge4.png", title: "Judul Tugas 4", link: "game4.html" },
